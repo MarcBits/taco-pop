fbcd04807f9cc99105dd47ceeb5f3971a35cfa4b
MarcBits <marcbits@outlook.com>
2016-12-14 11:27:51 -0500

    Use implemented protocols; added shakeable protocol.

--- TacoPOP/MainVC.swift
+++ TacoPOP/MainVC.swift
@@ -20,20 +20,26 @@ class MainVC: UIViewController, DataServiceDelegate {
         
         ds.delegate = self
         ds.loadDeliciousTacoData()
+        ds.tacoArray.shuffle()
         
         collectionView.delegate = self
         collectionView.dataSource = self
         
         headerView.addDropShadow()
         
+        /*
         let nib = UINib(nibName: "TacoCell", bundle: nil)
         collectionView.register(nib, forCellWithReuseIdentifier: "TacoCell")
+        */
+        
+        collectionView.register(TacoCell.self)
 
     }
     
     func deliciousTacoDataLoaded() {
         
         print("Delicious Taco Data Loaded!")
+        collectionView.reloadData()
     }
 
 }
@@ -52,19 +58,27 @@ extension MainVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollec
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TacoCell", for: indexPath) as? TacoCell {
-            
-            cell.configureCell(taco: ds.tacoArray[indexPath.row])
-            
-            return cell
-        }
-            
-        return UICollectionViewCell()
+//        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TacoCell", for: indexPath) as? TacoCell {
+//            
+//            cell.configureCell(taco: ds.tacoArray[indexPath.row])
+//            
+//            return cell
+//        }
+//            
+//        return UICollectionViewCell()
+        
+        let cell = collectionView.dequeueReusableCell(forIndexPath: indexPath) as TacoCell
+        cell.configureCell(taco: ds.tacoArray[indexPath.row])
+        
+        return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         
-        
+        if let cell = collectionView.cellForItem(at: indexPath) as? TacoCell {
+            
+            cell.shake()
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
